f7c9cafeb252bd5b20d6673848d76f786cb7a2b4
Ingram1314 <Ingram14@sjtu.edu.cn>
2020-07-08 22:50:14 -0700

    rm

--- Pokedex_1.03/Pokedex_1.03/app/src/main/java/com/example/pokedex/NotesDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.pokedex;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-//存储Note的数据库
-@Database(entities = {Note.class}, version = 1)
-public abstract class NotesDatabase extends RoomDatabase {
-    public abstract NoteDao noteDao();
-}
